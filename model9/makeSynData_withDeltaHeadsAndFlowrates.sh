240ed89891af170857d7259c346ffd07680b2ddf
testuser <testuser@master>
2020-04-15 04:10:54 +0200

    Renamed files

--- model9/makeSynData_withDeltaHeadsAndFlowrates.sh
+++ model9/makeSynData_withDeltaHeadsAndFlowrates.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-if [ -f synDataList_2.txt ]
-then
-	rm synDataList_2.txt
-fi
 
 # Deleting useless lines from synthetic head output file
-#sed -n '/0.3000000000000000E+03/,$p' ref_flow_obs_H_xyz.dat > temp.txt  #starting from this line
-sed -n '/0.0000000000000000E+00/,$p' ref_flow_obs_H_xyz.dat > temp.txt  #starting from this line
-cut -c25-263 temp.txt > gwSynHeads.txt #without the first column with times
-cut -c1-23 temp.txt > assimTimes.txt  #only the first column with times
+#sed -n '/0.3000000000000000E+03/,$p' ref_flow_obs_H_xyz.dat > temp.txt  # starting from this line
+sed -n '/0.0000000000000000E+00/,$p' ref_flow_obs_H_xyz.dat > temp.txt  # starting from this line
+sed -n '1~4p' temp.txt > temp2.txt
+cut -c25-263 temp2.txt > synDataByObsPts_2.txt # without the first column with times
+cut -c1-23 temp2.txt > assimTimes.txt  # only the first column with times
 
 # Preparing flowrate observations
 i=1
@@ -17,21 +14,16 @@ nbLayers=5
 while [ $i -le $nbLayers ]
 do
 	#deleting useless lines from synthetic flowrate output file
-	sed -n '/0.3000000000000000E+03/,$p' ref_flow_fluid_budget_$i.dat > temp_$i.txt
-	cut -c49-71 temp_$i.txt | awk '{ if($1>=0) {printf("%.4e\n",$1)} else {printf("%.4e\n",$1*-1)} }' > gwSynFlowrates$i.txt
+#	sed -n '/0.3000000000000000E+03/,$p' ref_flow_fluid_budget_$i.dat > temp_$i.txt
+	sed -n '8,27p' ref_flow_fluid_budget_$i.dat > temp_$i.txt	
+#	cut -c49-71 temp_$i.txt | awk '{ if($1>=0) {printf("%.4e\n",$1)} else {printf("%.4e\n",$1*-1)} }' > gwSynFlowrates$i.txt
+	cut -c49-71 temp_$i.txt | awk '{printf("%.4e\n",$1)}' > gwSynFlowrates$i.txt
 	((i=i+1))
 done
 cat $(printf gwSynFlowrates%d".txt " $(seq 1 $nbLayers)) > temp_synFlowrates.txt
 awk -F, '{printf("%.4e\n",$1)}' temp_synFlowrates.txt > synFlowrates.txt # reformat flowrates output value
 cp synFlowrates.txt flowrateObsData_withoutNoise.txt
-cp synFlowrates.txt flowrateObsData.txt
-
-# Making one file containing all synthetic data (10 first columns of head data + 5 last ones of flowrate data)
-paste -d' ' gwSynHeads.txt $(printf gwSynFlowrates%d".txt " $(seq 1 $nbLayers)) > synDataByObsPts.txt
-
-# Only keeping the head observations
-cut -c1-240 synDataByObsPts.txt > synDataByObsPts_2.txt
-
+./addRegularNoiseToSynObsData.py q
 
 # Preparing head observations
 
@@ -52,48 +44,53 @@ done
 awk -F, '{printf("%.4e\n",$1)}' temp_synHeads.txt > synHeads.txt
 rm temp*
 cp synHeads.txt headObsData_withoutNoise.txt
-cp synHeads.txt headObsData.txt
+./addRegularNoiseToSynObsData.py h
 
 # Preparing temporal head difference observations
 ini_index=1
 fin_index=24
 i=1
-while [ $i -le $nbObsPts ]
+while [ ${i} -le ${nbObsPts} ]
 do
-	cut -c$ini_index-$fin_index synDataByObsPts_2.txt | awk -F, '{printf("%.2f\n",$1)}' > temp
-	sed '1d' temp > headAfter_$i.dat # delete first line
+	cut -c${ini_index}-${fin_index} synDataByObsPts_2.txt | awk -F, '{printf("%.2f\n",$1)}' > temp
+	sed '1d' temp > headAfter_${i}.dat # delete first line
 	sed '$d' temp > headBefore_$i.dat # delete last line
-	paste -d' ' headAfter_$i.dat headBefore_$i.dat > headsForDiff_$i.dat
-	awk '{ headDiff = $1 - $2; printf "%.4e\n", headDiff }' headsForDiff_$i.dat > tempHeadDiffs_$i.dat
+	paste -d' ' headAfter_${i}.dat headBefore_${i}.dat > headsForDiff_${i}.dat
+	awk '{ headDiff = $1 - $2; printf "%.4e\n", headDiff }' headsForDiff_${i}.dat > tempHeadDiffs_${i}.dat
 	((ini_index=ini_index+24))
 	((fin_index=fin_index+24))
 	((i=i+1))
 done
-cat $(printf "tempHeadDiffs_%d.dat " $(seq 1 $nbObsPts)) > synDeltaHeads.txt  # Concatenate temporal head differences files
+cat $(printf "tempHeadDiffs_%d.dat " $(seq 1 ${nbObsPts})) > synDeltaHeads.txt  # Concatenate temporal head differences files
 rm headsForDiff* temp* headAfter* headBefore*
 cp synDeltaHeads.txt deltaHeadObsData_withoutNoise.txt
-cp synDeltaHeads.txt deltaHeadObsData.txt
+./addRegularNoiseToSynObsData.py dh
 
 # Preparing vertical head difference observations
 i=1 # counter for while loop; counter for vertHeadDiffs_*.dat files
 j=1 # counter of columns in synDataByObsPts_2.txt
 ((nbOfVerticalHeadDiffTimeSeries=nbObsPts-2)) # number of columns in future synVerticalHeadDiffs.txt file 
-while [ $i -le $nbOfVerticalHeadDiffTimeSeries ]
+while [ ${i} -le ${nbOfVerticalHeadDiffTimeSeries} ]
 do
 	((k=j+1))
 	if [ ${j} -eq 5 ] 
 	then
 		((j=j+1))
 	else
-		awk -F ' ' '{ print $'${k}' }' synDataByObsPts_2.txt > headAbove_$i.dat
-		awk -F ' ' '{ print $'${j}' }' synDataByObsPts_2.txt > headBelow_$i.dat	
-		paste -d' ' headAbove_$i.dat headBelow_$i.dat > headsForDiff_$i.dat
-		awk '{ headDiff = $1 - $2; printf "%.4e\n", headDiff }' headsForDiff_$i.dat > vertHeadDiffs_$i.dat
+		awk -F ' ' '{ print $'${k}' }' synDataByObsPts_2.txt > headAbove_${i}.dat
+		awk -F ' ' '{ print $'${j}' }' synDataByObsPts_2.txt > headBelow_${i}.dat	
+		paste -d' ' headAbove_${i}.dat headBelow_${i}.dat > headsForDiff_${i}.dat
+		awk '{ headDiff = $1 - $2; printf "%.4e\n", headDiff }' headsForDiff_${i}.dat > vertHeadDiffs_${i}.dat
 		((i=i+1))
 		((j=j+1))
 	fi
 done
-cat $(printf "vertHeadDiffs_%d.dat " $(seq 1 $nbOfVerticalHeadDiffTimeSeries)) > synVerticalHeadDiffs.txt  # Concatenate vertical head differences files
+cat $(printf "vertHeadDiffs_%d.dat " $(seq 1 ${nbOfVerticalHeadDiffTimeSeries})) > synVerticalHeadDiffs.txt  # Concatenate vertical head differences files
 rm headsForDiff* headAbove* headBelow* vertHeadDiffs*
 cp synVerticalHeadDiffs.txt vertHeadDiffObsData_withoutNoise.txt
-cp synVerticalHeadDiffs.txt vertHeadDiffObsData.txt
+./addRegularNoiseToSynObsData.py vdh
+
+# Make file of head observations by location
+./makeHeadObsByLocFile.py ${nbObsPts} # creates hObs_byLoc.txt
+./makeFlowrateObsByLocFile.py ${nbLayers} # creates qObs_byLoc.txt
+#cp hObs_byLoc.txt qObs_byLoc.txt ~/gwes # copy to main working directory
